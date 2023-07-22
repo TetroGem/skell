9d86d2dac45325e56bc323c0faf41062367d67cc
singletonb <singletonb@msoe.edu>
2023-07-22 18:20:46 -0500

    Compiled

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,4 @@
 export { Skell, capture, $capture } from './core/skell';
+export { Result } from './core/Result';
+export { Ok, Bad } from './core/shorthands';
 export { Numbers } from './namespaces/Numbers';
