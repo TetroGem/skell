c66a21d75934ea69e99360d82bbcfd882c3ce002
singletonb <singletonb@msoe.edu>
2023-07-22 18:22:06 -0500

    Fixed Result

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { Owned, Mutable, Readable, Skell, Type, Closure, $Closure, capture, $capture, Enum } from './core/skell';
-export { Result } from './core/Result';
+export { Result, $Result, $$Result } from './core/Result';
 export { Ok, Bad } from './core/shorthands';
 export { Numbers } from './namespaces/Numbers';
 export { DeepReadonly } from './types/DeepReadonly';
