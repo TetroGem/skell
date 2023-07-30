e94001ade1ab8a961ee0950c881847f912fee935
singletonb <singletonb@msoe.edu>
2023-07-29 22:08:13 -0500

    Compiled imports

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,6 @@
-export { Owned, Mutable, Readable, Skell, Type, Closure, $Closure, capture, $capture, Enum } from './core/skell';
+export { Owned, Mutable, Readable, Skell, Type, Closure, $Closure, capture, $capture, Enum, AbstractSkell } from './core/skell';
 export { Result, $Result, $$Result } from './core/Result';
+export { Vector2, $Vector2, $$Vector2 } from './core/Vector2';
 export { Ok, Bad } from './core/shorthands';
 export { Numbers } from './namespaces/Numbers';
 export { Immutable as DeepReadonly } from './types/Immutable';
--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 export { Skell, capture, $capture } from './core/skell';
 export { Result } from './core/Result';
+export { Vector2 } from './core/Vector2';
 export { Ok, Bad } from './core/shorthands';
 export { Numbers } from './namespaces/Numbers';
